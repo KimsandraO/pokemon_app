ad2851d8e3d472d226351001c4a2ee1aad40c302
Chamas111 <ahmad.chamas87@gmail.com>
2023-03-11 14:55:28 +0100

    edit on serverside

---
+++ server/index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const pokemonsRouter = require("./routes/pokemons");
+const path = require("path");
+const PORT = process.env.PORT || 9000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api/pokemons", pokemonsRouter);
+
+if (process.env.NODE_ENV === "production") {
+  const buildpath = path.join(__dirname, "../client/build");
+  app.use(express.static(buildpath));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(buildPath, "index.html"));
+  });
+}
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is runing on port ${PORT}`);
+  });
+});
