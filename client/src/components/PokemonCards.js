50087075d30ae6f7969603f671d9037ce2872280
rodrigomenezes22 <69732792+rodrigomenezes22@users.noreply.github.com>
2023-03-14 14:09:35 +0100

    Pokemon Cards, Pokemon Details, Pokemon Results

--- client/src/components/PokemonCards.js
+++ client/src/components/PokemonCards.js
@@ -1,7 +1,38 @@
 import React from "react";
+import { Link }  from "react-router-dom";
 
-function PokemonCards() {
-  return <div>PokemonsCards</div>;
+function PokemonCards({name, abilities, weakness, image, id}) {
+  return (
+    <div className="col-12 col-md-6 col-lg-3 mb-4">
+      <div className="pokemon-card lightgray-bg p-0 m-1">
+        <div className="header-card blue-bg p-1">
+          <h2 className="text-white">{name}</h2>
+
+        </div>
+        <div className="body-card">
+
+          <img src={image} alt={name} className="img-fluid thumbnail" />
+
+          <div className="container">
+            <div className="row">
+              <div className="col-6">
+                <h4>Abilities</h4>
+                <p>{abilities}</p>
+
+              </div>
+              <div className="col-6">
+                <h4>Weakness</h4>
+                <p>{weakness}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Link className="button-card yellow-bg" to={`/pokemons/${id}`}>
+        View Details</Link>
+      
+      </div>
+    </div>
+    )
 }
 
 export default PokemonCards;
--- client/src/components/PokemonDetails.js
+++ client/src/components/PokemonDetails.js
@@ -1,7 +1,92 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { useNavigate, useParams, Link } from "react-router-dom";
+import axios from "axios";
 
 function PokemonDetails() {
-  return <div>PokemonsDetails</div>;
+
+  const { pokemon_id } = useParams();
+  const [pokemon, setPokemon] = useState({});
+  const navigate = useNavigate();
+
+
+  console.log("whaaaaaaaaat")
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_SERVER_BASE_URL}/api/pokemons/${pokemon_id}`)
+      .then((res) => setPokemon(res.data))
+      .catch((e) => console.log(e));
+  }, [pokemon_id]);
+
+  return ( 
+    <div className="container" key={pokemon?._id}>
+    <div className="pokemon-card col-12 lightgray-bg p-0 mt-3 mb-5">
+      <div className="header-card blue-bg p-1">
+        <h2 className="text-white">{pokemon?.name}</h2>
+
+      </div>
+      <div className="body-card">
+        <div className="container">
+          <div className="row">
+            <div className="col-6 lightgray-bg">
+            <img src={pokemon?.image} alt={pokemon?.name} className="img-fluid" />
+            <p>There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.</p>
+
+            </div>
+            <div className="col-6 yellow-bg p-5">
+              <div className="row">
+                  <div className="col-12 col-lg-6">
+                    <h4>HEIGHT</h4>
+                    <p className="text-start"><span>{pokemon?.height}</span></p>
+                  </div>
+                  <div className="col-12 col-lg-6">
+                    <h4>WEIGHT</h4>
+                    <p className="text-start"><span>{pokemon?.weight}</span></p>
+                  </div>
+              </div>
+              <div className="row">
+                  <div className="col-12 col-lg-6">
+                    <h4>Abilities</h4>
+                    <p className="text-start">
+                      {pokemon.abilities?.map((e)=> <span>{e}</span>)}
+                    </p>
+                  </div>
+                  <div className="col-12 col-lg-6">
+                    <h4>Weakness</h4>
+                    <p className="text-start">
+                      {pokemon.weakness?.map((e)=> <span>{e}</span>)}
+                    </p>
+                  </div>
+              </div>
+              <div className="row">
+                  <div className="col-12 col-lg-6">
+                    <h4>Gender</h4>
+                    <p className="text-start">
+                    <span>{pokemon?.gender}</span>
+                    </p>
+                  </div>
+
+              </div>
+              <div className="row type-wrapper">
+
+                  <div className="col-6 text-center">
+                    <h4>Type</h4>
+                    <div className="text-center type-pokemon">
+                      {pokemon.type?.map((e)=> e ).join(", ")}
+                    </div>
+                  </div>
+              </div>
+
+            </div>
+          </div>
+          <div className="row bottom-details blue-bg"></div>
+      </div> 
+      </div>
+    </div>
+
+  </div>
+  );   
 }
 
 export default PokemonDetails;
