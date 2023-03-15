147f5335213b56c7e12ac63fb38d6a2858b10eab
Chamas111 <ahmad.chamas87@gmail.com>
2023-03-15 22:41:05 +0100

    api list

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import PokemonsCards from "./components/PokemonCards";
+
+import PokemonsApiDetails from "./components/PokemonApiDetails";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import AddForm from "./components/AddForm";
 import PokemonDetails from "./components/PokemonDetails";
 import UpdatePokemons from "./components/UpdatePokemons";
 import PokemonsApi from "./components/PokemonsApi";
-import Footer from "./components/Footer";
+
 import { Routes, Route } from "react-router-dom";
 import PokemonsResults from "./components/PokemonsResults";
 
@@ -20,15 +21,15 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/OurPokemons" element={<PokemonsResults />} />
           <Route path="/pokemons/new" element={<AddForm />} />
-          <Route path="/pokemons/:id" element={<PokemonDetails />} />
+          <Route path="/pokemons/publicPokemon" element={<PokemonsApi />} />
           <Route
-            path="/Pokemons/update/:id"
-            element={<UpdatePokemons />}
+            path="/pokemons/publicPokemon/:id"
+            element={<PokemonsApiDetails />}
           />
-          <Route path="/Pokemons/api/pokemons" element={<PokemonsApi />} />
+          <Route path="/pokemons/:id" element={<PokemonDetails />} />
+          <Route path="/Pokemons/update/:id" element={<UpdatePokemons />} />
         </Routes>
       </main>
-      <Footer />
     </div>
   );
 }
