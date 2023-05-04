0ed17fee1e5cbda2cdcd83a8b2896fd2b069ca2f
Chamas111 <ahmad.chamas87@gmail.com>
2023-05-04 19:16:19 +0200

    some edition

--- server/controllers/externalPokemons.js
+++ server/controllers/externalPokemons.js
@@ -1,30 +1,39 @@
 const getExtPokemons = async (req, res) => {
-  const { page = 1, limit = 20 } = req.query;
-  console.log(req.query);
+  // const { page = 1, limit = 20 } = req.query;
+  //console.log(req.query);
   const baseUrl = "https://pokeapi.co/api/v2/pokemon/";
-  const urls = [];
-  for (
-    let pokemons = (page - 1) * limit + 1;
-    pokemons <= page * limit;
-    pokemons++
-  ) {
-    urls.push(`${baseUrl}${pokemons}`);
-  }
+  // const urls = [];
+  // for (
+  //   let pokemons = (page - 1) * limit + 1;
+  //   pokemons <= page * limit;
+  //   pokemons++
+  // ) {
+  //   urls.push(`${baseUrl}${pokemons}`);
+  // }
+
+  // try {
+  //   const urlPokemons = await Promise.all(
+  //     urls.map((url) => fetch(url).then((response) => response.json()))
+  //   );
+  //   const pokemons = urlPokemons.map((urlPokemon) => {
+  //     return {
+  //       name: urlPokemon.name,
+  //       image: urlPokemon.sprites.other["official-artwork"].front_default,
+  //       _id: urlPokemon.id,
+  //     };
+  //   });
+  //   res.json(pokemons);
+  // } catch (error) {
+  //   console.log("Error", error);
+  // }
 
   try {
-    const urlPokemons = await Promise.all(
-      urls.map((url) => fetch(url).then((response) => response.json()))
-    );
-    const pokemons = urlPokemons.map((urlPokemon) => {
-      return {
-        name: urlPokemon.name,
-        image: urlPokemon.sprites.other["official-artwork"].front_default,
-        _id: urlPokemon.id,
-      };
-    });
-    res.json(pokemons);
+    const pokies = await fetch(baseUrl)
+      .then((response) => response.json())
+      .then((data) => data);
+    res.json(pokies);
   } catch (error) {
-    console.log("Error", error);
+    console.error(error.message);
   }
 };
 
